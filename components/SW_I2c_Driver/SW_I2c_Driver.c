474436bfaad46fc93b693ca06bde451d424eb3dd
majdirhim <54720363+majdirhim@users.noreply.github.com>
2023-03-16 18:14:55 +0100

    .gitignore

---
+++ components/SW_I2c_Driver/SW_I2c_Driver.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "SW_I2c_Driver.h"
+
+void func(void)
+{
+
+}
---
+++ main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+void app_main(void)
+{
+    while (true) {
+
+    }
+}
