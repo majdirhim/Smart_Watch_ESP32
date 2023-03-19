c4d4b8d96aa2f47c1b4aa685723579aae3540d09
majdirhim <54720363+majdirhim@users.noreply.github.com>
2023-03-19 19:05:39 +0100

    LIS2MDL_TASK for Mag and Temp Data

--- main/main.c
+++ main/main.c
@@ -31,7 +31,7 @@ SemaphoreHandle_t UART_Jeton =NULL;
 SemaphoreHandle_t I2c_Jeton =NULL;
 
 TaskHandle_t Lsm6dso_TASK_Handler ,StepCounter_Handler ;
-QueueHandle_t xQueue1;
+TaskHandle_t LIS2MDL_TASK_Handler;
 /************LIS2MDL Variables*********/
 static int16_t data_raw_magnetic[3];
 static int16_t data_raw_temperature;
@@ -55,11 +55,11 @@ void Lsm6dso_TASK(void * pvParameters ){
 	/**************One shot timer for entering sleep mode*************/
 	esp_timer_handle_t oneshot_timer;
 	const esp_timer_create_args_t oneshot_timer_args = {
-		  .callback = &oneshot_timer_callback,
-		  .name = "one-shot"
+			.callback = &oneshot_timer_callback,
+			.name = "one-shot"
 	};
 	esp_timer_create(&oneshot_timer_args, &oneshot_timer);
-    /**************One shot timer for entering sleep mode*************/
+	/**************One shot timer for entering sleep mode*************/
 	lsm6dso_all_sources_t all_source;
 	for(;;){
 		lsm6dso_all_sources_get(&Lsm6dso_dev_ctx, &all_source);
@@ -81,7 +81,7 @@ void StepCounter(void * pvParameters ){
 	uint16_t steps;
 	lsm6dso_emb_sens_t emb_sens;
 	lsm6dso_steps_reset(&Lsm6dso_dev_ctx);
-    /* Enable pedometer */
+	/* Enable pedometer */
 	lsm6dso_pedo_sens_set(&Lsm6dso_dev_ctx, LSM6DSO_FALSE_STEP_REJ_ADV_MODE);
 	emb_sens.step = PROPERTY_ENABLE;
 	emb_sens.step_adv = PROPERTY_ENABLE;
@@ -94,41 +94,73 @@ void StepCounter(void * pvParameters ){
 }
 
 
+
+
+
+void LIS2MDL_TASK(void * pvParameters){
+	uint8_t reg;
+	for(;;){
+		/* Read output only if new value is available */
+		lis2mdl_mag_data_ready_get(&Lis2mdl_dev_ctx, &reg);
+		if (reg) {
+			/* Read magnetic field data */
+			memset(data_raw_magnetic, 0x00, 3 * sizeof(int16_t));
+			lis2mdl_magnetic_raw_get(&Lis2mdl_dev_ctx, data_raw_magnetic);
+			magnetic_mG[0] = lis2mdl_from_lsb_to_mgauss( data_raw_magnetic[0]);
+			magnetic_mG[1] = lis2mdl_from_lsb_to_mgauss( data_raw_magnetic[1]);
+			magnetic_mG[2] = lis2mdl_from_lsb_to_mgauss( data_raw_magnetic[2]);
+			SW_SafePrint(&UART_Jeton,"Mag field [mG]:%4.2f\t%4.2f\t%4.2f\r\n",magnetic_mG[0], magnetic_mG[1], magnetic_mG[2]);
+
+			/* Read temperature data */
+			memset(&data_raw_temperature, 0x00, sizeof(int16_t));
+			lis2mdl_temperature_raw_get(&Lis2mdl_dev_ctx, &data_raw_temperature);
+			temperature_degC =lis2mdl_from_lsb_to_celsius(data_raw_temperature);
+			SW_SafePrint(&UART_Jeton,"Temperature [degC]:%6.2f\r\n",temperature_degC);
+		}
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+
+
+
+
 void app_main(void)
 {
 
-		  //UART Semaphore creation
-	      UART_Jeton=xSemaphoreCreateBinary();
-		  xSemaphoreGive(UART_Jeton);
-		  //I2c0 Semaphore Creation
-		  I2c_Jeton=xSemaphoreCreateBinary();
-	      xSemaphoreGive(I2c_Jeton);
+	//UART Semaphore creation
+	UART_Jeton=xSemaphoreCreateBinary();
+	xSemaphoreGive(UART_Jeton);
+	//I2c0 Semaphore Creation
+	I2c_Jeton=xSemaphoreCreateBinary();
+	xSemaphoreGive(I2c_Jeton);
 
-	      xTaskCreate(StepCounter, "StepCounter", 20000, NULL, 1, &StepCounter_Handler);
-	      vTaskSuspend(StepCounter_Handler);
-	      xTaskCreate(Lsm6dso_TASK, "Lsm6dso_TASK", 20000, NULL, 1, &Lsm6dso_TASK_Handler);
-	      vTaskSuspend(Lsm6dso_TASK_Handler);
-	      xQueue1 = xQueueCreate(3, sizeof(uint16_t) );
+	SW_I2c_Master_Init(I2C_NUM_0,19,18);
+	Lsm6dso_dev_ctx = SW_Mems_Interface_Init(I2C_NUM_0,0); //0=>Lsm6dso
+	Lis2mdl_dev_ctx = SW_Mems_Interface_Init(I2C_NUM_0,1);//1=>Lis2mdl
+	SW_Lsm6dso6_Init_Config(Lsm6dso_dev_ctx);
+	SW_Lis2mdl_Init_Config(Lis2mdl_dev_ctx);
 
-	      SW_I2c_Master_Init(I2C_NUM_0,19,18);
-	      Lsm6dso_dev_ctx = SW_Mems_Interface_Init(I2C_NUM_0,0); //0=>Lsm6dso
-	      Lis2mdl_dev_ctx = SW_Mems_Interface_Init(I2C_NUM_0,1);//1=>Lis2mdl
-	      SW_Lsm6dso6_Init_Config(Lsm6dso_dev_ctx);
-	      SW_Lis2mdl_Init_Config(Lis2mdl_dev_ctx);
-	      /**************LSM6DSO_INT1 ISR / Wake-up Trigger *****************/
-	      gpio_set_direction(LSM6DSO_INT1, GPIO_MODE_INPUT);
-	      gpio_set_intr_type(LSM6DSO_INT1,GPIO_INTR_POSEDGE);
-	      gpio_install_isr_service(0);
-	      gpio_isr_handler_add(LSM6DSO_INT1, Inactivity_Activity_IRQ,NULL);
-	      gpio_wakeup_enable(LSM6DSO_INT1, GPIO_INTR_HIGH_LEVEL);
-	      esp_sleep_enable_gpio_wakeup();
-	      /**************LSM6DSO_INT1 ISR / Wake-up Trigger *****************/
+	xTaskCreate(StepCounter, "StepCounter", 10000, NULL, 1, &StepCounter_Handler);
+	vTaskSuspend(StepCounter_Handler);
+	xTaskCreate(Lsm6dso_TASK, "Lsm6dso_TASK", 10000, NULL, 1, &Lsm6dso_TASK_Handler);
+	vTaskSuspend(Lsm6dso_TASK_Handler);
 
-		  /* Wait Events */
-		  while (1) {
-			  SW_SafePrint(&UART_Jeton,"MAIN TASK ORDO\n\r");
-			  vTaskDelay(1000/portTICK_PERIOD_MS);
-		  }
+	xTaskCreate(LIS2MDL_TASK, "LIS2MDL_TASK", 10000, NULL, 1, &LIS2MDL_TASK_Handler);
+
+	/**************LSM6DSO_INT1 ISR / Wake-up Trigger *****************/
+	gpio_set_direction(LSM6DSO_INT1, GPIO_MODE_INPUT);
+	gpio_set_intr_type(LSM6DSO_INT1,GPIO_INTR_POSEDGE);
+	gpio_install_isr_service(0);
+	gpio_isr_handler_add(LSM6DSO_INT1, Inactivity_Activity_IRQ,NULL);
+	gpio_wakeup_enable(LSM6DSO_INT1, GPIO_INTR_HIGH_LEVEL);
+	esp_sleep_enable_gpio_wakeup();
+	/**************LSM6DSO_INT1 ISR / Wake-up Trigger *****************/
+
+	/* Wait Events */
+	while (1) {
+		SW_SafePrint(&UART_Jeton,"MAIN TASK ORDO\n\r");
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
 }
 
 /**
@@ -144,11 +176,11 @@ esp_err_t SW_SafePrint(SemaphoreHandle_t* Jeton,const char* fmt, ...){
 	int ret_rtos= xSemaphoreTake(*Jeton,(TickType_t)1000/portTICK_PERIOD_MS);
 	if(!ret_rtos)
 		return ESP_ERR_TIMEOUT;
-    va_list arg;
-    va_start(arg, fmt);
-    vprintf(fmt, arg);
-    va_end(arg);
-    return !(xSemaphoreGive(*Jeton)); //return 0 if ok
+	va_list arg;
+	va_start(arg, fmt);
+	vprintf(fmt, arg);
+	va_end(arg);
+	return !(xSemaphoreGive(*Jeton)); //return 0 if ok
 }
 
 
